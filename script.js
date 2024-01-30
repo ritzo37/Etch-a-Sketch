f3754b1d1c81f68ff3ea1ae442344a60f5a41528
ritzo37 <ritishrai56@gmail.com>
2024-01-31 02:53:41 +0530

    Add hover effect in the grid

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const container = document.querySelector('.container');
 
-for (let i =0 ; i<10 ;i ++) {
+for (let i =0 ; i<100;i ++) {
     const outerDiv = document.createElement('div');
-    for (let j =0 ; j<10 ; j++) {
+    for (let j =0 ; j<100; j++) {
         const innerDiv = document.createElement('div');
         innerDiv.style.flex =  "1";
         innerDiv.style.height = "50px";
         innerDiv.style.widows=  "50px";
-        innerDiv.style.border= "2px solid black";
+        // innerDiv.style.border= "2px solid white";
+        innerDiv.classList.toggle("clickme")
         outerDiv.appendChild(innerDiv);
     }
     outerDiv.style.flex = "1" ;
@@ -15,3 +16,10 @@ for (let i =0 ; i<10 ;i ++) {
     outerDiv.style.flexDirection = "column";
     container.appendChild(outerDiv) ;
 }
+
+const grids = document.querySelectorAll('.clickme');
+grids.forEach((gridItem)=> {
+    gridItem.addEventListener('mouseenter',(e)=> {
+        e.target.style.backgroundColor = "hotpink";
+    })
+})
